92fbce258011235213581517e967e1880bb7c5a8
Michael Van de Ven <michael.vd.ven95@gmail.com>
2018-09-06 21:22:15 +0200

    Project generation

---
+++ my_admin/my_admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace my_admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
